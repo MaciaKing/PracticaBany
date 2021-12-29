7e45bca77635e5338d72d11cd4a9693fd1b99eaa
MaciaKing <51322831+MaciaKing@users.noreply.github.com>
2021-12-29 15:07:12 +0100

    .
    .

--- src/practicabany2/PracticaBany.java
+++ src/practicabany2/PracticaBany.java
@@ -12,27 +12,14 @@ import java.util.concurrent.Semaphore;
  * @author macia
  */
 public class PracticaBany {
-     
-//    static Semaphore grup3H = new Semaphore(3);
-//    static Semaphore grup3M = new Semaphore(3);
-//    static Semaphore grup3 = new Semaphore(3);
-
-//    static volatile int contadorPersonasWCH;
-//    static Semaphore ScontadorPersonasWCH = new Semaphore(1);
-//    static volatile int contadorPersonasWCM;
-//    static Semaphore ScontadorPersonasWCM = new Semaphore(1);
-    
+ 
     static Semaphore banyo = new Semaphore(1);
     static Semaphore cola = new Semaphore(1); //para la inanición
-    
-//    static int general;
+ 
   
 
     public static void main(String[] args) {       
-//        contadorPersonasWC=0;
-//        contadorPersonasWCH=0;
-//        contadorPersonasWCM=0;
-//        general=0;
+
         Thread m1 =new Thread(new Hombre("MACIA"));
         Thread m2 =new Thread(new Hombre("ARTURO"));
         Thread m3 =new Thread(new Hombre("JUAN"));
